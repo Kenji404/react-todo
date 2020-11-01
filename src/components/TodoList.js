e872bffd15b9ebf492d266533183f48c39d4b76a
Kenji404 <kenji.andre@gmail.com>
2020-11-01 15:02:57 -0300

    beta

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -16,32 +16,41 @@ function TodoList() {
         console.log(...todos);
     };
 
-    const removeTodo = id => {
-        const removeArr = [...todos].filter(todo => todo.id !== id)
+    const updateTodo = (todoId, newValue) => {
+        if (!newValue.text || /^\s*$/.test(newValue.text)) {
+            return;
+        }
 
-        setTodos(removeArr)
-    }
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+    };
+
+    const removeTodo = id => {
+        const removedArr = [...todos].filter(todo => todo.id !== id);
+
+        setTodos(removedArr);
+    };
 
     const completeTodo = id => {
         let updatedTodos = todos.map(todo => {
             if (todo.id === id) {
-                todo.isComplete = !todo.isComplete
+                todo.isComplete = !todo.isComplete;
             }
-            return todo
-        })
+            return todo;
+        });
         setTodos(updatedTodos);
-    }
+    };
 
     return (
-        <div>
-            <h1>What's the plan for today?</h1>
+        <>
+            <h1>What's the Plan for Today?</h1>
             <TodoForm onSubmit={addTodo} />
             <Todo
                 todos={todos}
-                compleTodo={completeTodo}
+                completeTodo={completeTodo}
                 removeTodo={removeTodo}
+                updateTodo={updateTodo}
             />
-        </div>
+        </>
     );
 }
 
