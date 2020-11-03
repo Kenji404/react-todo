8eda034392c128800708958d2af9ebb0eca16315
Kenji404 <kenji.andre@gmail.com>
2020-11-02 21:57:01 -0300

    Updates

---
+++ precache-manifest.5e6dc5c7a901f6f1369cd3dfa2a4905c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3446373b31587c4f34c99ced96c07f6f",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "6a9886a8c2b4f057c669",
+    "url": "/to-do/static/css/main.1ff72d68.chunk.css"
+  },
+  {
+    "revision": "bdeb0d8110971b5e9983",
+    "url": "/to-do/static/js/2.684a98c0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/to-do/static/js/2.684a98c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a9886a8c2b4f057c669",
+    "url": "/to-do/static/js/main.d9db9f20.chunk.js"
+  },
+  {
+    "revision": "b5d210bec7b313800160",
+    "url": "/to-do/static/js/runtime-main.2b5910fc.js"
+  }
+]);
